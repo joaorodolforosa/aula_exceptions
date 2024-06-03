102aabdeab369a8525ff9f5a19b6d5d932e7657c
Joao Rodolfo <joaorodolforosa@gmail.com>
2024-06-03 18:59:21 -0300

    Exemplo 3 Exceção gerada na chamada do método

--- src/br/uam/psc/application/Program.java
+++ src/br/uam/psc/application/Program.java
@@ -1,5 +1,7 @@
-// Exemplo de exceção capturada e tratada
+// Exemplo de exceção gerada na chamada do método
 package br.uam.psc.application;
+
+import br.uam.psc.TesteExcecao;
 
 /**
  *
@@ -9,16 +11,12 @@ public class Program {
 
     public static void main(String[] args) {
 
-        int nums[] = new int[4];
-
         try {
-            nums[7] = 10;
-
+            TesteExcecao.geradorDeExceção();
         } catch (ArrayIndexOutOfBoundsException ex) {
             System.out.println("Índice de array fora dos limite");
         }
-
-        System.out.println("Exemplo de um erro capturado e tratado");
+        System.out.println("Exemplo de uma exceção caturada e tratada na chamada do método");
     }
 
 }
