dd46de6bf9a3ce8ec2061808fe88e234f8bd7fd9
Joao Rodolfo <joaorodolforosa@gmail.com>
2024-06-03 19:25:36 -0300

    Exemplo 5 Exceção não capturada

--- src/br/uam/psc/application/Program.java
+++ src/br/uam/psc/application/Program.java
@@ -1,7 +1,5 @@
-// Exemplo de exceção gerada na chamada do método
+// Exemplo de exceção não capturada
 package br.uam.psc.application;
-
-import br.uam.psc.TesteExcecao;
 
 /**
  *
@@ -11,13 +9,14 @@ public class Program {
 
     public static void main(String[] args) {
 
+        int nums[] = new int[4];
+        
         try {
-            TesteExcecao.geradorDeExceção();
-        } catch (ArrayIndexOutOfBoundsException ex) {
+            nums[7] = 10;
+        } catch (ArithmeticException ex) {
             System.out.println("Índice de array fora dos limite");
-            ex.printStackTrace();
         }
-        System.out.println("Exemplo de uma exceção caturada e tratada na chamada do método");
+        System.out.println("Como a exceção não foi capturada este trecho não será executado");
     }
 
 }
